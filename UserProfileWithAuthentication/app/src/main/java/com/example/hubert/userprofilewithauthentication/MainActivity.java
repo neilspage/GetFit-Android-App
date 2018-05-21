52ff143e9aa3f8d4a08128078161076e4b52f14a
Hubert Chau <hubertchau97@gmail.com>
2018-05-21 15:43:10 +1000

    Add initial app

---
+++ UserProfileWithAuthentication/app/src/main/java/com/example/hubert/userprofilewithauthentication/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.hubert.userprofilewithauthentication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignOut,btnAddtoDatabase,btnViewDatabase;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    ProgressDialog PD;
+
+
+
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        PD = new ProgressDialog(this);
+        PD.setMessage("Loading...");
+        PD.setCancelable(true);
+        PD.setCanceledOnTouchOutside(false);
+
+        btnViewDatabase = (Button) findViewById(R.id.view_personal_details);
+        btnAddtoDatabase = (Button) findViewById(R.id.add_personal_details);
+        btnSignOut = (Button) findViewById(R.id.sign_out_button);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                auth.signOut();
+                FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        if (user == null) {
+                            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            finish();
+                        }
+                    }
+                };
+            }
+        });
+
+        findViewById(R.id.change_password_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ForgetAndChangePasswordActivity.class).putExtra("Mode", 1));
+            }
+        });
+
+        findViewById(R.id.change_email_button).setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ForgetAndChangePasswordActivity.class).putExtra("Mode", 2));
+            }
+        });
+
+        findViewById(R.id.delete_user_button).setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ForgetAndChangePasswordActivity.class).putExtra("Mode", 3));
+            }
+        });
+
+        btnAddtoDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddtoDatabase.class);
+                startActivity(intent);
+            }
+        });
+        btnViewDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewDatabase.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override    protected void onResume() {
+        if (auth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+        super.onResume();
+    }
+}
