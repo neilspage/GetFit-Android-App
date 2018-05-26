ae955985333cf0ab73c32c41d82fe672167d573c
Matthew Tria Delotavo <matthew.t.delotavo@student.uts.edu.au>
2018-05-27 00:17:20 +1000

    Remove floating action button

--- app/src/main/java/com/example/michelle/useraccount/WorkoutListActivity.java
+++ app/src/main/java/com/example/michelle/useraccount/WorkoutListActivity.java
@@ -55,15 +55,6 @@ public class WorkoutListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own actionX"+WorkoutFetcher.getWorkouts().get(0).getName(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         View recyclerView = findViewById(R.id.workout_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
